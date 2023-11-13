d9ab5c389c6dc4624744e7ff7d3ededf9393e1b2
LukaStjc <luka2001stajic@gmail.com>
2023-11-13 19:17:29 +0100

    Fixed 'blocked by CORS policy' problem

--- src/main/java/rs/ac/uns/ftn/informatika/jpa/controller/CompanyController.java
+++ src/main/java/rs/ac/uns/ftn/informatika/jpa/controller/CompanyController.java
@@ -3,6 +3,7 @@ package rs.ac.uns.ftn.informatika.jpa.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +19,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping(value = "api/companies")
+@CrossOrigin(origins = "http://localhost:3000")
 public class CompanyController {
 
     @Autowired
