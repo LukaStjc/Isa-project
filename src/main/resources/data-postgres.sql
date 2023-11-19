29fb6561ae451d4a6d90a681ee34bc0e6970137f
LukaStjc <luka2001stajic@gmail.com>
2023-11-19 12:32:52 +0100

    Filtering equipment is finished

--- src/main/resources/data-postgres.sql
+++ src/main/resources/data-postgres.sql
@@ -28,6 +28,7 @@ insert into exam (student_id, course_id, date, grade) values (2, 2, '2016-04-19'
 
 insert into location (city,country, street_name, street_number, longitude, latitude) values ('Serbia','Novi Sad' ,'Sime Milosevica',11, 19.833549, 45.267136);
 insert into location (city,country, street_name, street_number, longitude, latitude) values ('Serbia','Belgrade' ,'Pera Cetkovica',55, 20.833549, 46.267136);
+insert into location (city,country, street_name, street_number, longitude, latitude) values ('Serbia','Zajecar' ,'Jase Tomica',33, 20.833549, 46.267136);
 insert into hospital (name,location_id) values ('Bolnica Novi Sad', 1);
 
 insert into userr (id, email, first_name, last_name, password) values (5, 'sadas@gmail.com', 'Marko', 'Markovic', 'pass');
@@ -50,14 +51,20 @@ insert into registered_user (id , telephone_number, penalty_points, points, occu
 insert into registered_user (id , telephone_number, penalty_points, points, occupation,loyalty_program_id, hospital_id) values (8, '0613241802', 0, 0, 'poljoprivrednik',1, 1);
 -- insert into registered_user (id , telephone_number, penalty_points, points, occupation) values (5, '0613241802', 0, 0, 'zemljoradnik');
 
-insert into company (name,description, opening_time, closing_time, average_score, location_id, created_by_admin) values ('Kompanija A','opis1' ,'2020-01-01 07:00:00','2020-01-01 21:00:00', 0, 1, 7);
-insert into company (name,description, opening_time, closing_time, average_score, location_id, created_by_admin) values ('Belmex','kvalitetna medicinska oprema' ,'2020-01-01 07:00:00','2020-01-01 21:00:00', 0, 2, 7);
+-- TODO promeniti average_score na 0, za kt1 su stavljene neke vrednosti
+insert into company (name,description, opening_time, closing_time, average_score, location_id, created_by_admin) values ('Kompanija A','opis1' ,'2020-01-01 07:00:00','2020-01-01 21:00:00', 1.8, 1, 7);
+insert into company (name,description, opening_time, closing_time, average_score, location_id, created_by_admin) values ('Belmex','kvalitetna medicinska oprema' ,'2020-01-01 07:00:00','2020-01-01 21:00:00', 2.1, 2, 7);
+insert into company (name,description, opening_time, closing_time, average_score, location_id, created_by_admin) values ('Neomedica','sve za vas, od nas' ,'2020-01-01 07:00:00','2020-01-01 21:00:00', 3.3, 3, 7);
 
 
 insert into equipment (name,description, type, price, quantity, company_id) values ('Srafciger','alat za kucu' ,1  , 3000, 100, 1);
 insert into equipment (name,description, type, price, quantity, company_id) values ('Rendgen aprat x021','uredjaj za snimanje grudnog kosa', 2, 600000, 5, 1);
 insert into equipment (name,description, type, price, quantity, company_id) values ('Magnetna rezonanca p442','uredjaj za snimanje glave', 0, 700000, 6, 2);
-insert into equipment (name,description, type, price, quantity, company_id) values ('Kamerica 2mpx', 0, 0, 10000, 10, 2);
+insert into equipment (name,description, type, price, quantity, company_id) values ('Kamerica 2mpx', 'Za snimanje creva', 0, 10000, 10, 2);
+insert into equipment (name,description, type, price, quantity, company_id) values ('Mala kamera 3mpx', 'Za snimanje grla', 0, 9000, 25, 3);
+insert into equipment (name,description, type, price, quantity, company_id) values ('Srafciger','alat za majstorisanje' ,1  , 2500, 30, 3);
+insert into equipment (name,description, type, price, quantity, company_id) values ('Srafciger','alat za sve' ,1  , 2900, 80, 2);
+
 
 insert into company_admin (id,company_id, registered_by_admin) values (6, 1, 7);
 
