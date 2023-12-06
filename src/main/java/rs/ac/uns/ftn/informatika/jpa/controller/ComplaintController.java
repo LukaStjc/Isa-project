91a4d8f24099cabbbe0d20bbf08914d54de5762e
LukaStjc <luka2001stajic@gmail.com>
2023-12-06 08:41:39 +0100

    Finished getAll in ComplaintsController

--- src/main/java/rs/ac/uns/ftn/informatika/jpa/controller/ComplaintController.java
+++ src/main/java/rs/ac/uns/ftn/informatika/jpa/controller/ComplaintController.java
@@ -1,10 +1,18 @@
 package rs.ac.uns.ftn.informatika.jpa.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import rs.ac.uns.ftn.informatika.jpa.dto.ComplaintDTO;
+import rs.ac.uns.ftn.informatika.jpa.model.Complaint;
 import rs.ac.uns.ftn.informatika.jpa.service.ComplaintService;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "api/complaints")
@@ -15,5 +23,15 @@ public class ComplaintController {
     private ComplaintService complaintService;
 
 
+    @GetMapping
+    public ResponseEntity<List<ComplaintDTO>> getAll(){
+        List<Complaint> complaints = complaintService.getAll();
 
+        List<ComplaintDTO> complaintDTOS = new ArrayList<>();
+        for(Complaint c : complaints){
+            complaintDTOS.add(new ComplaintDTO(c));
+        }
+
+        return new ResponseEntity<>(complaintDTOS, HttpStatus.OK);
+    }
 }
--- src/main/java/rs/ac/uns/ftn/informatika/jpa/service/ComplaintService.java
+++ src/main/java/rs/ac/uns/ftn/informatika/jpa/service/ComplaintService.java
@@ -2,7 +2,11 @@ package rs.ac.uns.ftn.informatika.jpa.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import rs.ac.uns.ftn.informatika.jpa.dto.ComplaintDTO;
+import rs.ac.uns.ftn.informatika.jpa.model.Complaint;
 import rs.ac.uns.ftn.informatika.jpa.repository.ComplaintRepository;
+
+import java.util.List;
 
 @Service
 public class ComplaintService {
@@ -11,4 +15,7 @@ public class ComplaintService {
     private ComplaintRepository complaintRepository;
 
 
+    public List<Complaint> getAll() {
+        return complaintRepository.findAll();
+    }
 }
