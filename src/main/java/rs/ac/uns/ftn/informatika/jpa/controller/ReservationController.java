6248b27d9863a35e675dde6f5e24dbaa43c6cea0
LukaStjc <luka2001stajic@gmail.com>
2023-12-12 22:09:21 +0100

    Added getting reservations from the entire week

--- src/main/java/rs/ac/uns/ftn/informatika/jpa/controller/ReservationController.java
+++ src/main/java/rs/ac/uns/ftn/informatika/jpa/controller/ReservationController.java
@@ -22,10 +22,11 @@ public class ReservationController {
 
     @GetMapping
     public ResponseEntity<List<ReservationDTO>> getAllByDate
-            (@RequestParam("date") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSX") Date date) {
+            (@RequestParam("date") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSX") Date date,
+             @RequestParam("week") int showWeek) {
         //TODO dodati da se prikazuju samo one rezervacije koje su vezane za tog admina kompanije
 
-        List<ReservationDTO> reservationDTOS = reservationService.getAllByDate(date);
+        List<ReservationDTO> reservationDTOS = reservationService.getAllByDate(date, showWeek);
 
         return new ResponseEntity<>(reservationDTOS, HttpStatus.OK);
     }
--- src/main/java/rs/ac/uns/ftn/informatika/jpa/service/ReservationService.java
+++ src/main/java/rs/ac/uns/ftn/informatika/jpa/service/ReservationService.java
@@ -4,10 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import rs.ac.uns.ftn.informatika.jpa.dto.ReservationDTO;
 import rs.ac.uns.ftn.informatika.jpa.model.Reservation;
-import rs.ac.uns.ftn.informatika.jpa.repository.LocationRepository;
 import rs.ac.uns.ftn.informatika.jpa.repository.ReservationRepository;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
@@ -16,19 +16,80 @@ public class ReservationService {
     @Autowired
     private ReservationRepository reservationRepository;
 
-    public List<ReservationDTO> getAllByDate(Date date){
+    public List<ReservationDTO> getAllByDate(Date date, int showWeek){
         List<Reservation> reservations = reservationRepository.findAll();
 
-        int day = date.getDate();
-        int month = date.getMonth();
-        int year = date.getYear();
+        final int day = date.getDate();
+        final int month = date.getMonth();
+        final int year = date.getYear(); // getDay() 0-nedelja, 1-ponedeljak ... 6-subota
 
         List<ReservationDTO> reservationDTOS = new ArrayList<>();
-        for(Reservation r : reservations){
-            if(r.getStartingDate().getDate() == day && r.getStartingDate().getMonth() == month
-                && r.getStartingDate().getYear() == year){
 
-                reservationDTOS.add(new ReservationDTO(r));
+        if(showWeek == 0){
+            for(Reservation r : reservations){
+                if(r.getStartingDate().getDate() == day && r.getStartingDate().getMonth() == month
+                        && r.getStartingDate().getYear() == year){
+
+                    reservationDTOS.add(new ReservationDTO(r));
+                }
+            }
+        }
+        else{   //getDay() -> ako je 3. dan, u nazad se ide 2 -> 3-1=2, u napred se ide 4 -> 7-3=4, ako je 0. dan, to je 7. dan
+            int dayInWeek = date.getDay();
+            if(dayInWeek == 0) dayInWeek = 7;
+
+            int daysBefore = dayInWeek - 1;
+            int daysAfter = 7 - dayInWeek;
+
+            Calendar calendar = Calendar.getInstance();
+
+            // Dodajem rezervacije koje su u nedelji pre izabranog dana
+            for(int i=daysBefore; i>0; --i){
+                calendar.setTime(date);
+
+                calendar.add(Calendar.DAY_OF_MONTH, -i);
+
+                Date tmpDate = calendar.getTime();
+                int tmpDay = tmpDate.getDate();
+                int tmpMonth = tmpDate.getMonth();
+                int tmpYear = tmpDate.getYear();
+
+                for(Reservation r : reservations){
+                    if(r.getStartingDate().getDate() == tmpDay && r.getStartingDate().getMonth() == tmpMonth
+                            && r.getStartingDate().getYear() == tmpYear){
+
+                        reservationDTOS.add(new ReservationDTO(r));
+                    }
+                }
+            }
+
+            // Dodajem rezervacije koje su na izabrani dan
+            for(Reservation r : reservations){
+                if(r.getStartingDate().getDate() == day && r.getStartingDate().getMonth() == month
+                        && r.getStartingDate().getYear() == year){
+
+                    reservationDTOS.add(new ReservationDTO(r));
+                }
+            }
+
+            // Dodajem rezervacije koje su u nedelji posle izabranog dana
+            for(int i=0; i< daysAfter; ++i){
+                calendar.setTime(date);
+
+                calendar.add(Calendar.DAY_OF_MONTH, i);
+
+                Date tmpDate = calendar.getTime();
+                int tmpDay = tmpDate.getDate();
+                int tmpMonth = tmpDate.getMonth();
+                int tmpYear = tmpDate.getYear();
+
+                for(Reservation r : reservations){
+                    if(r.getStartingDate().getDate() == tmpDay && r.getStartingDate().getMonth() == tmpMonth
+                            && r.getStartingDate().getYear() == tmpYear){
+
+                        reservationDTOS.add(new ReservationDTO(r));
+                    }
+                }
             }
         }
 
